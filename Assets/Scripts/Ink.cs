f53e0d920b225da70c59c3be305955f91df5c2e0
Anthony Cloudy <acloudy@mail.smu.edu>
2015-10-08 11:17:49 -0500

    Added click handling for our ink sprite

--- Assets/Scripts/Ink.cs
+++ Assets/Scripts/Ink.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections;
 
-public class Ink : MonoBehaviour {
+public class Ink : MonoBehaviour
+{
+	public bool mouseDown = false;
+	public bool mouseOver = false;
 
-	public int buttCount = 0;
 	// Use this for initialization
 	void Start () 
 	{
@@ -13,6 +16,27 @@ public class Ink : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		if (mouseDown && mouseOver) 
+		{
+
+		}
+	}
 	
+	public void OnMouseDown()
+	{
+		mouseDown = true;
+	}
+	
+	public void OnMouseUp()
+	{
+		mouseDown = false;
+	}
+
+	void OnMouseEnter() {
+		mouseOver = true;
+	}
+	
+	void OnMouseExit() {
+		mouseOver = false;
 	}
 }
