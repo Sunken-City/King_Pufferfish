5cebd261a066a630895914718c286c59c1101591
Anthony Cloudy <acloudy@mail.smu.edu>
2015-10-08 11:53:09 -0500

    Ink blobs now fade based on scrub strength

--- Assets/Scripts/Ink.cs
+++ Assets/Scripts/Ink.cs
@@ -4,29 +4,50 @@ using System.Collections;
 
 public class Ink : MonoBehaviour
 {
-	public bool mouseDown = false;
 	public bool mouseOver = false;
+
+	static bool mouseDown = false;
+	static Vector2 previousMousePosition;
+	static Camera sceneCamera;
 	// Use this for initialization
 	void Start () 
 	{
-		
+		sceneCamera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		StartCoroutine ("GetPreviousMousePosition");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (mouseDown && mouseOver) 
+		if (mouseDown) 
 		{
-			Color inkColor = GetComponent<SpriteRenderer>().color;
-			inkColor.a -= 0.01f;
-			GetComponent<SpriteRenderer>().color = inkColor;
-			if (inkColor.a <= 0.0f) 
+			Vector2 currentMousePosition = (Vector2)sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+			if(mouseOver)
 			{
-				Destroy(this.gameObject);
+				Color inkColor = GetComponent<SpriteRenderer>().color;
+
+				Vector2 mouseDistanceTravelledThisFrame = currentMousePosition - previousMousePosition;
+				float swipeStrength = mouseDistanceTravelledThisFrame.magnitude / 10.0f;
+				Debug.Log(currentMousePosition + " - " + previousMousePosition + " = " + swipeStrength);
+				inkColor.a -= swipeStrength;
+				GetComponent<SpriteRenderer>().color = inkColor;
+				if (inkColor.a <= 0.0f) 
+				{
+					Destroy(this.gameObject);
+				}
 			}
 		}
 	}
-	
+
+	public IEnumerator GetPreviousMousePosition()
+	{
+		while (true) 
+		{
+			previousMousePosition = (Vector2)sceneCamera.ScreenToWorldPoint (Input.mousePosition);
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+
 	public void OnMouseDown()
 	{
 		mouseDown = true;
