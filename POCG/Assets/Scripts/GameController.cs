f82c894b449cc743e1ed9e8f7941238349ca533b
Anthony Cloudy <acloudy@mail.smu.edu>
2015-11-04 16:59:43 -0600

    Added POCG milestone

---
+++ POCG/Assets/Scripts/GameController.cs
@@ -0,0 +1,272 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public Wave[] waves;
+    public float defaultEnemySpeed = 1.0f;
+    public float difficultySpeedMultiplier = 1.0f;
+    public float difficultySpeedIncrementPerWave = 0.02f;
+    public float difficultySpeedMultiplierCap = 1.5f;
+    public float rageSpawnMultiplier = 2.0f;
+    public float rageSpeedMultiplier = 1.5f;
+    public AudioClip ambientSound;
+    public GameObject soundPlayer;
+    public int numberOfBeginningWavesToRemove = 2;
+
+    [HideInInspector]
+    public int score = 0;
+    [HideInInspector]
+    public bool scrubLock;
+    [HideInInspector]
+    public bool rageMode = false;
+    [HideInInspector]
+    public float rageDuration = 0.0f;
+
+    private float timeSinceLastSpawn = 0.0f;
+    private float timeUntilNextSpawn = 2.0f;
+    private int currentWaveIndex = 0;
+    private int currentEncounterIndex = 0;
+    private bool currentWaveFinished = false;
+    private GameObject ambientPlayer;
+    private float screenShakeMagnitude = 0.0f;
+    private Vector3 defaultCameraPosition;
+
+    void Awake()
+    {
+        //If we have a second instance, destroy it first
+        if (instance)
+        {
+            Debug.Log("Destroying irrelevant GameController instance");
+            Destroy(instance.gameObject);
+        }
+
+        instance = this;
+        defaultCameraPosition = Camera.main.transform.position;
+    }
+
+    void OnDestroy()
+    {
+        Destroy(ambientPlayer);
+        StopAllCoroutines();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        timeSinceLastSpawn = 0.0f;
+        Debug.Log("Starting wave #" + currentWaveIndex);
+        ambientPlayer = Instantiate(soundPlayer, Vector3.zero, Quaternion.identity) as GameObject;
+        AudioSource ambientSoundPlayer = ambientPlayer.GetComponent<AudioSource>();
+        ambientSoundPlayer.clip = ambientSound;
+        ambientSoundPlayer.loop = true;
+        ambientSoundPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+           // RageForSeconds(3.0f);
+        }
+        float adjustedTimeSinceFrame = Time.deltaTime * (rageMode ? rageSpawnMultiplier : 1.0f);
+        timeSinceLastSpawn += adjustedTimeSinceFrame;
+        if (currentWaveIndex < waves.Length)
+        {
+            if (timeUntilNextSpawn <= timeSinceLastSpawn)
+            {
+                SpawnFromWave();
+            }
+            if (currentWaveFinished)
+            {
+                currentWaveIndex++;
+                difficultySpeedMultiplier += difficultySpeedIncrementPerWave;
+                if (difficultySpeedMultiplier > difficultySpeedMultiplierCap)
+                {
+                    difficultySpeedMultiplier = difficultySpeedMultiplierCap;
+                }
+                currentEncounterIndex = 0;
+                currentWaveFinished = false;
+            }
+        }
+        else
+        {
+            Debug.Log("Out of waves, randomizing!");
+            ShuffleWaves(waves);
+            numberOfBeginningWavesToRemove = 0;
+            currentWaveIndex = 0;
+        }
+        Vector3 cameraOffset = new Vector3(Random.Range(0.0f, screenShakeMagnitude), Random.Range(0.0f, screenShakeMagnitude), 0.0f);
+        Camera.main.transform.position = defaultCameraPosition + cameraOffset;
+
+        screenShakeMagnitude -= Time.deltaTime;
+        if (screenShakeMagnitude < 0.0f)
+        {
+            screenShakeMagnitude = 0.0f;
+        }
+    }
+
+    public void AddScore(int scoreIncrement)
+    {
+        score += scoreIncrement;
+        Debug.Log("score is now :" + score);
+    }
+
+    private void SpawnFromWave()
+    {
+        Wave currentWave = waves[currentWaveIndex];
+        if (currentEncounterIndex < currentWave.EncounterList.Length)
+        {
+            Debug.Log("Spawning Encounter #" + currentEncounterIndex);
+            Encounter currentEncounter = currentWave.EncounterList[currentEncounterIndex];
+
+            //Calculate speed constant
+            float enemySpeed = currentEncounter.speed;
+            if (enemySpeed == 0.0f)
+            {
+                enemySpeed = defaultEnemySpeed;
+            }
+            enemySpeed *= difficultySpeedMultiplier;
+            enemySpeed *= rageMode ? rageSpeedMultiplier : 1.0f;
+
+            //Calculate the spawn position and velocity based on where the enemy is going to move.
+            Vector3 spawnPosition = new Vector3(0.0f, 0.0f, 0.0f);
+            Vector2 enemyVelocity = new Vector2(0.0f, 0.0f);
+            if (currentEncounter.movementDirection == MovementDirection.DOWN)
+            {
+                spawnPosition = new Vector3(currentEncounter.spawnPositionOffset, 14.0f, 0.0f);
+                enemyVelocity = new Vector2(0.0f, -enemySpeed);
+            }
+            else if (currentEncounter.movementDirection == MovementDirection.LEFT_TO_RIGHT)
+            {
+                spawnPosition = new Vector3(-4.5f, currentEncounter.spawnPositionOffset + 8.0f, 0.0f);
+                enemyVelocity = new Vector2(enemySpeed, 0.0f);
+            }
+            else if (currentEncounter.movementDirection == MovementDirection.RIGHT_TO_LEFT)
+            {
+                spawnPosition = new Vector3(4.5f, currentEncounter.spawnPositionOffset + 8.0f, 0.0f);
+                enemyVelocity = new Vector2(-enemySpeed, 0.0f);
+            }
+
+            if (!currentEncounter.enemy)
+            {
+                currentWaveFinished = true;
+                Debug.LogWarning("Current wave cut short, missing a gameobject for element " + currentEncounterIndex);
+                return;
+            }
+
+            GameObject enemy = Instantiate(currentEncounter.enemy, spawnPosition, Quaternion.identity) as GameObject;
+            enemy.GetComponent<Rigidbody2D>().velocity = enemyVelocity;
+
+            if (enemy.GetComponent<Squid>())
+            {
+                enemy.transform.localEulerAngles = new Vector3(0.0f, 0.0f, 180.0f);
+            }
+            else if (rageMode && enemy.GetComponent<UnderwaterMine>())
+            {
+                Destroy(enemy);
+                Debug.Log("Mine prevented from spawning because of rage mode");
+            }
+          
+            timeSinceLastSpawn = 0.0f;
+            timeUntilNextSpawn = currentEncounter.waitSecondsAfterSpawn;
+            currentEncounterIndex++;
+        }
+        else
+        {
+            currentWaveFinished = true;
+            Debug.Log("Wave Finished");
+        }
+    }
+
+    public void PlaySound(AudioClip sound)
+    {
+        StartCoroutine(PlaySoundAsync(sound));
+    }
+
+    public IEnumerator PlaySoundAsync(AudioClip sound)
+    {
+        GameObject tempSoundPlayer = Instantiate(soundPlayer, Vector3.zero, Quaternion.identity) as GameObject;
+        AudioSource player = tempSoundPlayer.GetComponent<AudioSource>();
+        player.clip = sound;
+        float timeToPlay = 0.0f;
+        if (sound != null)
+        {
+            timeToPlay = player.clip.length;
+            player.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Sound was not attached for a PlaySound call");
+        }
+        yield return new WaitForSeconds(timeToPlay);
+        Destroy(tempSoundPlayer);
+    }
+
+    //Built off of http://www.vcskicks.com/randomize_array.php
+    private void ShuffleWaves(Wave[] waves)
+    {
+        int randomIndex = 0;
+        ArrayList currentWaves = new ArrayList(waves);
+        for (int i = 0; i < numberOfBeginningWavesToRemove; i++)
+        {
+            currentWaves.RemoveAt(i);
+        }
+        ArrayList randomizedWaves = new ArrayList();
+        while (currentWaves.Count > 0)
+        {
+            randomIndex = Random.Range(0, currentWaves.Count); //Choose a random object in the list
+            randomizedWaves.Add(currentWaves[randomIndex]); //add it to the new, random list
+            currentWaves.RemoveAt(randomIndex); //remove to avoid duplicates
+        }
+
+        int index = 0;
+        waves = new Wave[waves.Length - numberOfBeginningWavesToRemove];
+        foreach(Wave wave in randomizedWaves.ToArray())
+        {
+            waves[index++] = wave;
+        }
+    }
+
+    public void RageForSeconds(float seconds)
+    {
+        rageDuration = seconds;
+        StartCoroutine(SetRage(seconds));
+    }
+
+    public IEnumerator SetRage(float seconds)
+    {
+        rageMode = true;
+        yield return new WaitForSeconds(seconds);
+        rageMode = false;
+    }
+
+    public void ReleaseScrubLock()
+    {
+        StartCoroutine(ReleaseScrubLockCoroutine());
+    }
+
+    public IEnumerator ReleaseScrubLockCoroutine()
+    {
+        while (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("Not yet released");
+            yield return new WaitForEndOfFrame();
+        }
+        GameController.instance.scrubLock = false;
+        Debug.Log("Released");
+    }
+
+    public void ShakeScreen(float magnitude)
+    {
+        if (screenShakeMagnitude > magnitude)
+        {
+            return;
+        }
+        screenShakeMagnitude = magnitude;
+    }
+}
---
+++ POCG/Assets/Scripts/Ink.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Ink : MonoBehaviour
+{
+	public bool mouseOver = false;
+	//Higher resistance means harder to scrub.
+	public float inkScrubResistance = 10.0f;
+
+	static bool mouseDown = false;
+	static Vector2 previousMousePosition;
+    static Camera sceneCamera;
+    public AudioClip SqueakForward;
+    public AudioClip SqueakBack;
+
+    private static int inkCount = 0;
+    private static float timeSinceLastSqueak = 0.0f;
+    private bool squeakForward = false;
+
+    void Awake()
+    {
+        inkCount++;
+        timeSinceLastSqueak = Time.time;
+    }
+
+	// Use this for initialization
+	void Start () 
+	{
+		sceneCamera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		//We start up a coroutine that runs in the background, grabbing the mouse's new position every 1/10th of a second.
+		StartCoroutine ("GetPreviousMousePosition");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (Input.GetKey(KeyCode.Mouse0)) 
+		{
+			Vector2 currentMousePosition = (Vector2)sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+			if(mouseOver)
+			{
+				Color inkColor = GetComponent<SpriteRenderer>().color;
+				//Take our current mouse position and subtract the previous position to get a vector between the two
+				Vector2 mouseDistanceTravelledThisFrame = currentMousePosition - previousMousePosition;
+                bool scrubForward = squeakForward;
+                if (mouseDistanceTravelledThisFrame.x + mouseDistanceTravelledThisFrame.y > 0)
+                {
+                    scrubForward = true;
+                }
+                else if (mouseDistanceTravelledThisFrame.x + mouseDistanceTravelledThisFrame.y < 0)
+                {
+                    scrubForward = false;
+                }
+                if (scrubForward != squeakForward)
+                {
+                    squeakForward = scrubForward;
+                    timeSinceLastSqueak = Time.time;
+                    if (squeakForward)
+                    {
+                        GameController.instance.PlaySound(SqueakForward);
+                    }
+                    else
+                    {
+                        GameController.instance.PlaySound(SqueakBack);
+                    }
+                }
+				//Divide the magnitude by the resistance value. Subtract this value from the ink's alpha value
+                float swipeStrength = mouseDistanceTravelledThisFrame.magnitude / inkScrubResistance;
+				inkColor.a -= swipeStrength;
+                GameController.instance.scrubLock = true;
+				SprayBulletScript.ChargeUp(swipeStrength);
+				GetComponent<SpriteRenderer>().color = inkColor;
+				if (inkColor.a <= 0.0f) 
+				{
+					Destroy(this.gameObject);
+				}
+			}
+		}
+        else
+        {
+            GameController.instance.scrubLock = false;
+        }
+	}
+
+    public void OnDestroy()
+    {
+        inkCount--;
+        if (inkCount == 0 && GameController.instance)
+        {
+            GameController.instance.ReleaseScrubLock();
+        }
+    }
+
+	public IEnumerator GetPreviousMousePosition()
+	{
+		while (true) 
+		{
+			previousMousePosition = (Vector2)sceneCamera.ScreenToWorldPoint (Input.mousePosition);
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+
+	public void OnMouseDown()
+	{
+		mouseDown = true;
+	}
+	
+	public void OnMouseUp()
+	{
+		mouseDown = false;
+        GameController.instance.scrubLock = false;
+	}
+
+	void OnMouseEnter() {
+		mouseOver = true;
+	}
+	
+	void OnMouseExit() {
+		mouseOver = false;
+	}
+}
